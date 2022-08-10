5e934c3077a5d36134d51a8cb7f784bbb83086b7
songexile <therunningbeta@gmail.com>
2022-08-10 17:33:29 +1200

    Styled dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,32 @@
 import React from "react";
+import Bar from "../img/bar.png";
+import Bar2 from "../img/bar2.png";
+import Bar3 from "../img/bar3.jpg";
+import Pie from "../img/pie.png";
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
-      <div className="dashboard--title">
-        <h2 className="dashboard--large">Dashboard</h2>
-        <p>Take a look at your graphs</p>
-      </div>
       <div className="dashboard--content">
+        {/* Represents one row */}
         <div class="row">
-          <div class="column">adg</div>
-          <div class="column">adg</div>
+          <div class="column ">
+            <img src={Bar} />
+          </div>
+          <div class="column ">
+            {" "}
+            <img src={Bar2} />
+          </div>
+        </div>
+        <div class="row">
+          <div class="column ">
+            {" "}
+            <img src={Bar3} />
+          </div>
+          <div class="column ">
+            {" "}
+            <img src={Pie} />
+          </div>
         </div>
       </div>
     </div>
