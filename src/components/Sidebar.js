94e54e00d19b8ffc33d4fe9187f982d3b5f8eb1e
songexile <therunningbeta@gmail.com>
2022-08-20 13:24:11 +1200

    Added links in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,46 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar--1">
-        <h1>Dashboard</h1>
+        <h1>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            Dashboard
+          </Link>
+        </h1>
         <div className="sidebar--children">
-          <h2>Racetrack</h2>
-          <h2>Cumulative</h2>
-          <h2>Relay</h2>
-          <h2>Dissociative</h2>
-          <h2>Flow</h2>
-          <h2>OPS average</h2>
+          <h2>
+            <Link to="g-racetrack" style={{ textDecoration: "none" }}>
+              Racetrack
+            </Link>
+          </h2>
+          <h2>
+            <Link to="g-cumulative" style={{ textDecoration: "none" }}>
+              Cumulative
+            </Link>
+          </h2>
+          <h2>
+            <Link to="g-relay" style={{ textDecoration: "none" }}>
+              Relay
+            </Link>
+          </h2>
+          <h2>
+            <Link to="g-dissociative" style={{ textDecoration: "none" }}>
+              Dissociative
+            </Link>
+          </h2>
+          <h2>
+            <Link to="g-flow" style={{ textDecoration: "none" }}>
+              Flow
+            </Link>
+          </h2>
+          <h2>
+            <Link to="g-ops" style={{ textDecoration: "none" }}>
+              OPS average
+            </Link>
+          </h2>
         </div>
       </div>
       <div className="sidebar--2">
